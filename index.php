6abb719ea4a2d1113187a5127fea58c06078914b
Luan Schmitz <luanfgdo@hotmail.com>
2022-06-04 18:13:51 -0300

    create elements

---
+++ index.php
@@ -0,0 +1,210 @@
+<!doctype html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="mobile-web-app-capable" content="yes">
+        <meta name="theme-color" content="#ffffff"/>
+        <title>Santins</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+        <link rel="icon" href="/imgs/favicon.png" type="image/png" sizes="100x100">
+        <!-- Og tags -->
+        <meta name="title" content="Santins">
+        <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur ad quo dolorem quia reiciendis saepe a ipsam, eligendi, adipisci nulla obcaecati neque dolore fugiat laborum cumque consequuntur vero eum accusantium.">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://localhost:8080/">
+        <meta property="og:title" content="Santins">
+        <meta property="og:description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur ad quo dolorem quia reiciendis saepe a ipsam, eligendi, adipisci nulla obcaecati neque dolore fugiat laborum cumque consequuntur vero eum accusantium.">
+        <meta property="og:image" content="https://localhost:8080/imgs/social-share.png"> 
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content="https://localhost:8080">
+        <meta property="twitter:title" content="Santins">
+        <meta property="twitter:description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur ad quo dolorem quia reiciendis saepe a ipsam, eligendi, adipisci nulla obcaecati neque dolore fugiat laborum cumque consequuntur vero eum accusantium.">
+        <meta property="twitter:image" content="https://localhost:8080/imgs/social-share.png">
+    </head>
+    <body>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-9 col-md-3">
+                        <a href="#"><img src="#" alt="Santins Logo"></a>
+                    </div>
+                    <div class="col-3 col-md-9">
+                        <nav>
+                            <button type="button">
+                                togle
+                            </button>
+                            <div class="nav-home text-end">
+                                <a href="#">A EMPRESA</a>
+                                <a href="#">NOSSOS FUNDOS</a>
+                                <a href="#">CONTEÚDOS</a>
+                                <a href="#">CONTATO</a>
+                                <a href="#">FAÇA LOGIN</a>
+                                <a href="#">INVISTA AGORA</a>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-8 col-lg-6">
+                        <h1>SANTINS</h1>
+                        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit...</h2>
+                        <a href="#">SAIBA MAIS</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="col-12 col-md-6 offset-md-6">
+                    <h2>A empresa</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis ducimus temporibus architecto repellat vero ullam, est maiores. Quam maxime numquam accusamus nam eum, atque eligendi quia explicabo, itaque, ratione quaerat. <b>Lorem ipsum dolor sit amet consectetur adipisicing elit.</b> Autem consequuntur delectus molestiae dicta nam laudantium pariatur quaerat quia commodi, voluptate qui deleniti, esse sit! Repellat ea nihil itaque voluptas iusto?</p>
+                    <a href="#">SABER MAIS</a>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-6 col-md-4 col-lg-2">
+                        <div class="card">
+                            <p><span>4</span></p>
+                            <p>Lorem</p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4 col-lg-2">
+                        <div class="card">
+                            <p><span>100</span><small>%</small></p>
+                            <p>Lorem</p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4 col-lg-2">
+                        <div class="card">
+                            <p><span>10</span></p>
+                            <p>Lorem</p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4 col-lg-2">
+                        <div class="card">
+                            <p><span>162,6</span><small>%</small></p>
+                            <p>Lorem</p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4 col-lg-2">
+                        <div class="card">
+                            <p><span>1°</span></p>
+                            <p>Lorem</p>
+                        </div>
+                    </div>
+                    <div class="col-6 col-md-4 col-lg-2">
+                        <div class="card">
+                            <p><small>R$</small><span>162,6</span><small>MM</small></p>
+                            <p>Lorem</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-7">
+                        <div class="row">
+                            <div class="col-12 col-md-10 col-lg-10">
+                                <h2>Fundos</h2>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores quaerat nisi asperiores, veniam officiis ipsa eaque repudiandae aspernatur assumenda aperiam omnis voluptatibus totam necessitatibus ad ullam fugit? A, dignissimos animi!</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 ms-auto">
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse eligendi accusantium sint laboriosam temporibus? Deserunt alias quaerat laboriosam dicta sit est qui dolorem quod perferendis, ratione, optio facere, enim adipisci?</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <h2>Fundos</h2>
+                    <div class="col-12 col-md-6">
+                        <img src="#" alt="Fundo 1">
+                        <h3>FUNDO 1</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat quibusdam, dolore nam labore provident, explicabo vel facere blanditiis amet beatae magni tempore voluptates. Nihil tenetur quaerat necessitatibus exercitationem aliquid vel.</p>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <img src="#" alt="Fundo 2">
+                        <h3>FUNDO 2</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat quibusdam, dolore nam labore provident, explicabo vel facere blanditiis amet beatae magni tempore voluptates. Nihil tenetur quaerat necessitatibus exercitationem aliquid vel.</p>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <img src="#" alt="Fundo 3">
+                        <h3>FUNDO 3</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat quibusdam, dolore nam labore provident, explicabo vel facere blanditiis amet beatae magni tempore voluptates. Nihil tenetur quaerat necessitatibus exercitationem aliquid vel.</p>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <img src="#" alt="Fundo 4">
+                        <h3>FUNDO 4</h3>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quaerat quibusdam, dolore nam labore provident, explicabo vel facere blanditiis amet beatae magni tempore voluptates. Nihil tenetur quaerat necessitatibus exercitationem aliquid vel.</p>
+                    </div>
+                    <div class="col-12">
+                        <a href="#">SABER MAIS</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-md-10 col-lg-8 text-center">
+                        <h2>Inscreva-se na nossa Newsletter</h2>
+                        <form action="#" class="row">
+                            <input type="email" name="" id="" class="col-12 col-md-8">
+                            <input type="submit" value="Assinar Newsletter" class="col-12 col-md-4">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-md-4">
+                        <h2>Santins</h2>
+                        <img src="#" alt="Logo Santins">
+                        <p>Lorem ipsum</p>
+                        <ul class="social">
+                            <li><a href="#">face</a></li>
+                            <li><a href="#">insta</a></li>
+                            <li><a href="#">twitter</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-md-8">
+                        <div class="row">
+                            <div class="col-12 col-md-4">
+                                <h3>Menu</h3>
+                                <ul>
+                                    <li><a href="#">A empresa</a></li>
+                                    <li><a href="#">Nossos fundos</a></li>
+                                    <li><a href="#">Conteúdos</a></li>
+                                    <li><a href="#">Contato</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-12 col-md-4">
+                                <h3>Contato</h3>
+                                <a href="#">contato@santins.com.br</a>
+                            </div>
+                            <div class="col-12">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ipsam voluptatum unde esse quos veniam autem quod pariatur, beatae architecto inventore eaque assumenda illum distinctio dolores ratione accusamus cum? Neque?</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <p>2022 Santins</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    </body>
+</html>
